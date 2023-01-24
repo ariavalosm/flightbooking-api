2b5e653b39b2ead51313aaf13cb7e9c6f141de82
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-24 17:47:26 -0500

    init

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//routes
+app.get("/", (req,res) =>{
+  res.send("hello,world");
+});
+
 // starting the server
 app.listen(app.get("port"), () => {
   console.log(`Server on port ${app.get("port")}`);
