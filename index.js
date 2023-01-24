b249e0d3f247eef5ea9ab66b3317081b83954a06
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-24 17:38:08 -0500

    init

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = required ("morgan");
 
 // middlewares(
 app.use(morgan("combined"));
-
 
 // starting the server
 app.listen(3000, () => {
