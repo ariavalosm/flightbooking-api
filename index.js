ca7355a2b68ca1cb18c2934cd04655fe173f7676
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-26 14:39:53 -0500

    init

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 //settings
 app.set('port', process.env.PORT || 3000);
 
 // middlewares
+app.use(morgan('combined'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
