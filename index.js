71b42fadfe65f007c6dadd70673bf2928756c3a3
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-26 14:37:29 -0500

    init

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = required("morgan");
 
 //settings
-app.set("port", process.env.PORT || 3000);
-app.set(express.json());
+app.set('port', process.env.PORT || 3000);
 
 // middlewares
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //routes
 app.use(require('./routes'));
 app.use('/api/datos', require('./routes/datos'));
+app.use('/api/ciudades', require('./routes/ciudades'));
 
 // starting the server
-app.listen(app.get("port"), () => {
-  console.log(`Server on port ${app.get("port")}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
 });
