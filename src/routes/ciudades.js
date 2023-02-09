9a1de1094d0e107d0743a22197bf96f3b0b233e9
Arianna Avalos <arianna.avalos15@gmail.com>
2023-02-09 17:41:02 -0500

    added

--- src/routes/ciudades.js
+++ src/routes/ciudades.js
@@ -4,52 +4,13 @@ const _ = require('underscore');
 
 const trips = require("../trip.json");
 
-router.get("/", (req,res) =>{
-  res.json(trips);
-});
+router.get("/", );
 
-router.post("/", (req,res) =>{
-  const { id } = req.params;
-  const { desde, hacia, fechaSalida, fechaRetorno } = req.body;
-  const newTrip = {... req.body, id}
-  if (![desde, hacia, fechaSalida, fechaRetorno].includes("")){
-    trips.push(newTrip);
-    res.json(trips);
-  } else {
-    res.status(500).json({error : "hay un error"});
-  }
+router.post("/",);
 
-});
-
-router.put('/:id', (req, res) =>{
-  const { id } = req.params;
-  const { desde, hacia, fechaSalida, fechaRetorno } = req.body;
-  if  (desde && hacia && fechaSalida && fechaRetorno){
-  _.each(trips, (trip, i) =>{
-      if (trips.id === id) {
-        trip.desde = desde;
-        trip.hacia = hacia;
-        trip.fechaSalida = fechaSalida;
-        trip.fechaRetorno = fechaRetorno;
-    }
-  });
-      res.json(trips);
-  } else {
-      res.status(500).json({error : "hay un error"});
-  }
-  });
+router.put('/:id', );
 
 
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  if (id){
-    _.each(trips, (trip, i) =>{
-      if (trip.id == id){
-        trips.splice(i ,1);
-  } 
-});
-  res.json(trips);
-}
-});
+router.delete("/:id", );
 
-module.exports = router;
+export default router;
