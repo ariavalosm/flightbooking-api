ffbeb1d7ce4aaaaa5f88ce8956e8cfeaf9eb05c2
Arianna Avalos <arianna.avalos15@gmail.com>
2023-02-10 10:49:53 -0500

    added

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import express from "express";
 import cors from "cors";
 
+import ciudadesRoutes from "./src/routes/ciudadesRoutes";
+import datosRoutes from "./src/routes/datosRoutes";
+
+var app = express();
+
+app.use(cors());
+
+app.use("/ciudades", ciudadesRoutes);
+app.use("/datos", datosRoutes);
+
+export default app
